2d61971f8903f23bb8b62b7b6927107a2f8eb6e0
Philippe Marchand <marchand.philippe@gmail.com>
2016-05-02 14:33:31 -0400

    Append SLURM_JOB_ID to filename instead of folder

--- r-test-file.R
+++ r-test-file.R
@@ -14,6 +14,6 @@ surveys_summary <- data.frame(species=levels(surveys_complete$species),
                               sd_wgt=species_sd,
                               min_wgt=species_min,
                               max_wgt=species_max)
-pdf(file.path(Sys.getenv('OUTPUT_DIR'),"mean_per_species.pdf"))
+pdf(paste0('mean_per_species_', Sys.getenv('SLURM_JOB_ID'), '.pdf'))
 barplot(surveys_summary$mean_wgt)
 dev.off()
